02798e2f7aed65783fffa6760d9c9b52a363b58e
guohui.huang <guo7892000@126.com>
2022-07-11 00:28:08 +0800

    五种数据库测试都通过

--- Breezee.AutoSQLExecutor.Test/Program.cs
+++ Breezee.AutoSQLExecutor.Test/Program.cs
@@ -6,9 +6,20 @@ using System.Data;
 using Newtonsoft.Json;
 
 DbServerInfo dbServer = new DbServerInfo();
+//连接SqlServer：
 //dbServer.DbType(DataBaseType.SqlServer).Server("localhost").User("sa").Pwd("sa123456").Port("1443").Db("PeachBase");//测试通过
+
+//连接MariaDB数据库：
 //dbServer.DbType(DataBaseType.MySql).Server("localhost").User("root").Pwd("root").Port("3306").Db("mis_main");//测试通过
-dbServer.DbType(DataBaseType.SQLite).Server(@"C:\Users\Administrator\Desktop\SQLite_MISFramework.db");//测试通过
+
+//连接MSQLite数据库：主要是文件形式
+//dbServer.DbType(DataBaseType.SQLite).Server(@"C:\Users\Administrator\Desktop\SQLite_MISFramework.db");//测试通过
+
+//连接Oralce数据库：数据库在Win7的虚拟机中，这里使用主机名
+//dbServer.DbType(DataBaseType.Oracle).Server("//USER-20220625SI:1521/orcl").User("hui").Pwd("hui");//测试通过,不知为啥用orcl的TNS名会报错？？
+
+//连接PostgreSQL数据库
+dbServer.DbType(DataBaseType.PostgreSql).Server("localhost").Db("MISMain").User("postgres").Pwd("postgres");//测试通过
 
 IDictionary<string, string> dicQuery = new Dictionary<string, string>();
 DataTable dt;
@@ -20,8 +31,9 @@ string sSql = "SELECT * FROM BAS_CITY T WHERE T.CITY_ID = @CITY_ID";
 
 //
 //sSql = "SELECT * FROM BAS_CITY T WHERE T.CITY_ID = #CITY_ID#";
-sSql = "SELECT * FROM sys_dll T WHERE T.DLL_ID = #DLL_ID#";//SQL中没有发现键会报错，是否允许？？配置输出SQL日志？？
-dt = AutoSQLExecutor.Connect(dbServer).QueryAutoParamData(sSql, dicQuery);//测试通过
+//dicQuery["TYPE_ID"] = "0015da9e-593a-45fd-826d-9ebe26500f4d";
+sSql = "SELECT * FROM BAS_TYPE T WHERE T.TYPE_ID = '#TYPE_ID#'";//
+dt = AutoSQLExecutor.Connect(dbServer).QueryAutoParamData(sSql, dicQuery);
 
 Console.WriteLine(dt.Rows.Count);
 Console.WriteLine(JsonConvert.SerializeObject(dt.Rows[0].ItemArray));
